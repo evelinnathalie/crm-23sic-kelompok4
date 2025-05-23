d190a559077edd3ee18e70faa4b38c8e6d694af3
Jill586 <faziel23si@mahasiswa.pcr.ac.id>
2025-05-23 09:30:59 +0700

    selesai menu penjualan by faziel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes,Route } from "react-router-dom";
 import MainLayout from "./Components/MainLayout";
 import Dashboard from "./Pages/Dashboard";
+import SalesManagement from "./Pages/SalesManagement";
 
 function App() {
   return(
     <Routes>
       <Route element={<MainLayout />}>
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Dashboard />}></Route>
+      <Route path="/penjualan" element={<SalesManagement />} />
       </Route>
     </Routes>
   )
